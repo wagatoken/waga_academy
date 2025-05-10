d454ebf9f400fd44c9104faa4e619ea029ec6e94
Dagmawi Solomon <02dagmawisolomon@gmail.com>
2025-05-10 17:28:00 +0300

    feat: Add client-side rendering

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { HeroSection } from "@/components/hero-section"
 import { FeaturedCourses } from "@/components/featured-courses"
 import { ValueChainSection } from "@/components/value-chain-section"
